82f03403fdca2299264d0e2a69ea16db2fa74c6e
Elena <eparapounsky@gmail.com>
2025-02-21 10:26:24 -0800

    improve formatting of output

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import zmq
+import json
 
 # zeromq 'client'
 context = zmq.Context()
@@ -6,34 +7,39 @@ socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555") # connect to the remote socket
 
 # get a recipe with a valid recipe id
-print("Getting recipe with valid ID, '1' ")
-socket.send_json({"recipeID": "1"})
+print("-"*100)
+print("\nGetting recipe with valid ID, '2' ")
+socket.send_json({"recipeID": "2"})
 response = socket.recv_json()
-print(f"Microservice's response: {response}")
+print(f"Microservice's response: {json.dumps(response, indent=2)}")
 
 # try to get a recipe without a valid recipe id
-print("Getting recipe with invalid ID, '50' ")
+print("-"*100)
+print("\nGetting recipe with invalid ID, '50' ")
 socket.send_json({"recipeID": "50"})
 response = socket.recv_json()
-print(f"Microservice's response: {response}")
+print(f"Microservice's response: {json.dumps(response, indent=2)}")
 
 # search for a recipe with a query that has matches
-print("Searching for recipe with query that has matches, 'pasta' ")
+print("-"*100)
+print("\nSearching for recipe with query that has matches, 'pasta' ")
 socket.send_json({"searchQuery": "pasta"})
 response = socket.recv_json()
-print(f"Microservice's response: {response}")
+print(f"Microservice's response: {json.dumps(response, indent=2)}")
 
 # search for a recipe with a query that doesn't have matches
-print("Searching for recipe with query that doesn't have matches, 'pizza' ")
+print("-"*100)
+print("\nSearching for recipe with query that doesn't have matches, 'pizza' ")
 socket.send_json({"searchQuery": "pizza"})
 response = socket.recv_json()
-print(f"Microservice's response: {response}")
+print(f"Microservice's response: {json.dumps(response, indent=2)}")
 
 # send an invalid request
-print("Sending an invalid request, 'none' ")
+print("-"*100)
+print("\nSending an invalid request, 'none' ")
 socket.send_json({"none": "none"})
 response = socket.recv_json()
-print(f"Microservice's response: {response}")
+print(f"Microservice's response: {json.dumps(response, indent=2)}")
 
 socket.close() # close socket
 context.term() # close context
