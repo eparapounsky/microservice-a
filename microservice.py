aa3c3eb91d6c94ea5fe4e9bdf425f30c4a833aa6
Elena <eparapounsky@gmail.com>
2025-02-20 18:01:39 -0800

    begin implementing main server loop

--- microservice.py
+++ microservice.py
@@ -6,7 +6,7 @@ filepath = "./recipes.json"
 
 def get_recipes(filepath):
     with open(filepath, 'r') as recipe_file:
-        return json.load(recipe_file)
+        return json.load(recipe_file) # returns list of recipes as dictionaries
 
 recipes = get_recipes(filepath)
 
@@ -14,3 +14,15 @@ recipes = get_recipes(filepath)
 context = zmq.Context()
 socket = context.socket(zmq.REP) # reply socket
 socket.bind("tcp://*:5555") # the socket will listen on network port 5555
+
+# main server loop
+while True:
+    request = socket.recv_json() # wait for requests from client in json format
+
+    # get parameters from request
+    recipe_id = request.get("recipeID")
+    search_query = request.get("searchQuery")
+
+    # get recipe by recipe id
+
+    # search recipes by search query
