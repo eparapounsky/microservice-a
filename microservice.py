39a4c1a8a45338453e4ce26fd7f4c841c854165c
Elena <eparapounsky@gmail.com>
2025-02-20 17:21:10 -0800

    zeromq server

--- microservice.py
+++ microservice.py
@@ -7,3 +7,10 @@ filepath = "./recipes.json"
 def get_recipes(filepath):
     with open(filepath, 'r') as recipe_file:
         return json.load(recipe_file)
+
+recipes = get_recipes(filepath)
+
+# zeromq server
+context = zmq.Context()
+socket = context.socket(zmq.REP) # reply socket
+socket.bind("tcp://*:5555") # the socket will listen on network port 5555
