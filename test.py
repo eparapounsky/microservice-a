937c2e2bf5deda2dd22fcc991c0d4e20d6884bab
Elena <eparapounsky@gmail.com>
2025-02-20 19:49:48 -0800

    outline tests

---
+++ test.py
@@ -0,0 +1,9 @@
+# get a recipe with a valid recipe id
+
+# try to get a recipe without a valid recipe id
+
+# search for a recipe with a query that has matches
+
+# search for a recipe with a query that doesn't have matches
+
+# send an invalid request
